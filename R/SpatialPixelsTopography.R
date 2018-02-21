fe70e23e3c4238f674ccf1e6b282b4ca9d2a1924
Miquel De Cáceres <miquelcaceres@gmail.com>
2018-02-21 10:33:15 +0100

    New constructor SpatialPixelsTopography

--- R/SpatialPixelsTopography.R
+++ R/SpatialPixelsTopography.R
@@ -1,23 +1,23 @@
-SpatialPointsTopography<-function(points, elevation, slope, aspect, proj4string = CRS(as.character(NA))) {
+SpatialPixelsTopography<-function(points, elevation, slope, aspect, tolerance = sqrt(.Machine$double.eps), 
+                                  proj4string = CRS(as.character(NA)), round = NULL, grid = NULL) {
   if(!(inherits(points,"SpatialPoints")|| inherits(points,"matrix"))) stop("'points' has to be of class 'matrix' or 'SpatialPoints'.")
   if(inherits(points,"SpatialPoints")) {
     npoints = nrow(points@coords)
-    proj4string =points@proj4string
-    coords = points@coords
-    bbox = points@bbox
   } else {
-    coords  = as.matrix(points)
-    npoints = nrow(coords)
-    bbox = bbox(SpatialPoints(coords))
+    npoints = nrow(as.matrix(points))
   }
   if(length(elevation)!=npoints) stop("'elevation' has to be of length equal to the number of points")
   if(length(slope)!=npoints) stop("'slope' has to be of length equal to the number of points")
   if(length(aspect)!=npoints) stop("'aspect' has to be of length equal to the number of points")
   data = data.frame(elevation = elevation, slope = slope, aspect = aspect)
-  lt = new("SpatialPointsTopography",
-          coords = coords,
-          bbox = bbox,
-          proj4string = proj4string,
-          data = data)
+  spdf = SpatialPixelsDataFrame(points, data, tolerance, proj4string, round, grid)
+  lt = new("SpatialPixelsTopography",
+          coords = spdf@coords,
+          coords.nrs = spdf@coords.nrs,
+          bbox = spdf@bbox,
+          grid = spdf@grid,
+          grid.index = spdf@grid.index,
+          proj4string = spdf@proj4string,
+          data = spdf@data)
   return(lt)
 }

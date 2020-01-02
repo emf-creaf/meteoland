bf712d0672aa734d2f663abe586fe6a5126e7aa2
Miquel De Cáceres <miquelcaceres@gmail.com>
2020-01-02 13:25:20 +0100

    mergepoints

--- R/mergegrids.R
+++ R/mergegrids.R
@@ -60,39 +60,60 @@ mergepoints<-function(..., verbose = TRUE) {
   mg_1 = l[[1]]
   dates = mg_1@dates
   vars = names(mg_1@data[[1]])
-  cc = mg_1@coords
+  ccmax = mg_1@coords
   for(i in 2:ng) {
     mg_i <- l[[i]]
     vars_i = names(mg_i@data[[1]])
     if(!identicalCRS(mg_1,mg_i)) stop("CRSs have to be identical.")
     dates = c(dates, mg_i@dates)
     vars = c(vars, vars_i)
-    cc = rbind(cc, mg_i@coords)
+    ccmax = rbind(ccmax, mg_i@coords)
   }
   dates = sort(unique(dates))
   nt = length(dates)
   vars = unique(vars)
-  npts= nrow(cc)
-  rownames(cc)<-1:npts
-  data = vector("list", npts)
+  nptsmax= nrow(ccmax)
+  newPts = 0
+  cc = matrix(nrow=0, ncol=2)
+  colnames(cc) = colnames(ccmax)
+  data = vector("list", nptsmax)
   if(verbose) pb = txtProgressBar(1, ng, style=3)
-  cnt = 0
   for(j in 1:ng) {
+    if(verbose) setTxtProgressBar(pb, j)
     mg_j = l[[j]]
     npt_j = nrow(mg_j@coords)
     for(i in 1:npt_j) {
-      cnt = cnt + 1
-      if(verbose) setTxtProgressBar(pb, cnt)
-      m = matrix(NA, nrow = nt, ncol = length(vars))
-      df = as.data.frame(m, row.names=1:nt)
-      names(df) <-vars
-      row.names(df)<-as.character(dates)
       df_i = mg_j@data[[i]]
-      df[row.names(df_i),names(df_i)] = df_i
-      data[[cnt]] = df
-      names(data)[cnt] = names(mg_j@data)[i]
+      #Check if coordinates are new
+      cc_i = mg_j@coords[i,]
+      if(nrow(cc)>0) {
+        w = which(cc[,1]==cc_i[1] & cc[,2]==cc_i[2])
+        isnew = (length(w)==0)
+      } else {
+        isnew = T
+      }
+      if(isnew) {
+        newPts = newPts+1
+        #Add coords
+        cc = rbind(cc, cc_i)
+        #New data frame
+        m = matrix(NA, nrow = nt, ncol = length(vars))
+        df = as.data.frame(m, row.names=1:nt)
+        names(df) <-vars
+        row.names(df)<-as.character(dates)
+        df[row.names(df_i),names(df_i)] = df_i
+        data[[newPts]] = df
+      } else {
+        #Old data frame
+        df = data[[w]]
+        df[row.names(df_i),names(df_i)] = df_i
+        data[[w]] = df
+      }
     }
   }
+  data = data[1:newPts]
+  rn = rownames(cc)
+  if(length(unique(rn))<length(rn)) rownames(cc) = 1:newPts
   spm = SpatialPointsMeteorology(points = SpatialPoints(cc, proj4string = mg_1@proj4string),
                                  data = data, dates = dates)
   return(spm)

9aa8cec5ad59b146b0d1eba5ad8321980a404fa7
MalditoBarbudo <victorgrandagarcia@gmail.com>
2022-11-22 08:43:54 +0100

    added tests for humidity_specific2relative

--- tests/testthat/test-05_meteoland_utils.R
+++ tests/testthat/test-05_meteoland_utils.R
@@ -1,6 +1,7 @@
 temperature <- seq(5, 30, 5)
 relative_humidity <- seq(50, 100, 10)
 dew_temperature <- seq(6, 1, -1)
+specific_humidity <- seq(0.00266, 0.0266, length.out = 6)
 
 test_that("humidity_relative2dewtemperature works as expected", {
 
@@ -47,7 +48,35 @@ test_that("humidity_dewtemperature2relative works as expected", {
 
   # error when no numbers
   expect_error(
-    humidity_relative2dewtemperature(letters, LETTERS)
+    humidity_dewtemperature2relative(letters, LETTERS)
   )
 
+})
+
+test_that("humidity_specific2relative works as expected", {
+  # vector
+  expect_type(
+    (test_res <- humidity_specific2relative(temperature, specific_humidity)),
+    "double"
+  )
+  expect_length(test_res, length(temperature))
+
+  # matrix
+  temperature_matrix <- matrix(temperature, nrow = 2, ncol = 3)
+  specific_humidity_matrix <- matrix(specific_humidity, nrow = 2, ncol = 3)
+  expect_no_error(
+    (test_res_matrix <- humidity_specific2relative(temperature_matrix, specific_humidity_matrix))
+  )
+  expect_true(inherits(test_res_matrix, "matrix"))
+  expect_equal(dim(test_res_matrix), c(2,3))
+  expect_identical(test_res, as.vector(test_res_matrix))
+
+  # it should be no value above 100
+  expect_false(any(test_res > 100))
+  expect_true(any(humidity_specific2relative(temperature, specific_humidity, TRUE) > 100))
+
+  # error when no numbers
+  expect_error(
+    humidity_specific2relative(letters, LETTERS)
+  )
 })
